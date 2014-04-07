3eba2f9f469e1ebebd78950454cd34332b445fea
Mattias Andrée <maandree@operamail.com>
2014-04-07 10:43:12 +0200

    complete code
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/unicorn.c
+++ src/unicorn.c
@@ -27,6 +27,7 @@
 #include <dirent.h>
 
 
+
 /**
  * The maximum length of PATH
  */
@@ -51,6 +52,7 @@
 #endif
 
 
+
 /**
  * This is the main entry point of the program
  * 
@@ -66,15 +68,16 @@ int main(int argc, char** argv)
   char* real_home = getpwuid(getuid()) ? getpwuid(getuid())->pw_dir : NULL;
   
   /* Unified PATH */
-  char unicorn_path[MAX_PATH];
+  char unicorn_path_env[5 + MAX_PATH];
+  char* unicorn_path = unicorn_path_env + 5;
   
   /* Get the largest possible PATH */
   #define _bin(DIR)			\
     ":" DIR "/bin"   ":" DIR "/xbin"	\
     ":" DIR "/sbin"  ":" DIR "/sxbin"
   
-  snprintf(unicorn_path, MAX_PATH,
-           "%s"
+  snprintf(unicorn_path_env, 5 + MAX_PATH,
+           "PATH=%s"
            _bin("/usr/local")  _bin("/usr/local/games")
            _bin("/usr")        _bin("/usr/games")
            _bin("")            _bin("/games")
@@ -232,8 +235,183 @@ int main(int argc, char** argv)
 	  /* Close the directory */
 	  closedir(dir);
 	}
+    }
+  else
+    {
+      int preserve_env = 0;
+      char* command_file = NULL;
+      char** command_args;
+      int command_argc;
+      char* command;
+      int i;
       
-      return 0;
+      /* Parse command line options */
+      for (i = 1; i < argc; i++)
+	{
+	  char* arg = argv[i];
+	  
+	  if (*arg != '-')
+	    break;
+	  else if (!strcmp("-p", arg) || !strcmp("--preserve-env", arg))
+	    preserve_env = 1;
+	  else if (!strcmp("-h", arg) || !strcmp("--help", arg))
+	    {
+	      printf("%s [-p] [--] command...\n\n%s\n",
+		     *argv,
+		     "    -p (--preserve-env)    Do not update PATH in the environment\n"
+		     "\n"
+		     "    -h (--heelp)           Print this help\n"
+		     "    -c (--copyright)       Print copyright information\n"
+		     "    -w (--warranty)        Print warranty disclaimer");
+	      return 0;
+	    }
+	  else if (!strcmp("-c", arg) || !strcmp("--copying", arg) || !strcmp("--copyright", arg))
+	    {
+	      printf("%s\n",
+		     "unicorn – PATH unification utility\n"
+		     "\n"
+		     "Copyright © 2014  Mattias Andrée (maandree@member.fsf.org)\n"
+		     "\n"
+		     "This program is free software: you can redistribute it and/or modify\n"
+		     "it under the terms of the GNU General Public License as published by\n"
+		     "the Free Software Foundation, either version 3 of the License, or\n"
+		     "(at your option) any later version.\n"
+		     "\n"
+		     "This program is distributed in the hope that it will be useful,\n"
+		     "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+		     "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+		     "GNU General Public License for more details.\n"
+		     "\n"
+		     "You should have received a copy of the GNU General Public License\n"
+		     "along with this program.  If not, see <http://www.gnu.org/licenses/>.");
+	      return 0;
+	    }
+	  else if (!strcmp("-w", arg) || !strcmp("--warranty", arg))
+	    {
+	      printf("%s\n",
+		     "This program is distributed in the hope that it will be useful,\n"
+		     "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+		     "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+		     "GNU General Public License for more details.");
+	      return 0;
+	    }
+	  else if (!strcmp("--", arg))
+	    {
+	      i++;
+	      break;
+	    }
+	  else
+	    fprintf(stderr, "%s: warning: option was not recognised: %s\n", *argv, arg);
+	}
+      if (i == argc)
+	{
+	  fprintf(stderr, "%s: error: no command specified\n", *argv);
+	  return 1;
+	}
+      command = *(command_args = argv + i);
+      command_argc = argc - i;
+      
+      /* Use the path of the command if it was specified */
+      if (strchr(command, '/'))
+	command_file = command;
+      
+      /* Look for the command in our PATH if we did not get its path */
+      if (command_file == NULL)
+	{
+	  /* The end of our PATH */
+	  char* end = unicorn_path + strlen(unicorn_path) + 1;
+	  /* The end of the current directory */
+	  char* p_end;
+	  /* The current directory */
+	  char* p;
+	  
+	  /* The pathname of the found file */
+	  char pathname[MAX_PATH];
+	  
+	  for (p = unicorn_path; (p != end) && (command_file == NULL); p = p_end + 1)
+	    {
+	      DIR* dir;
+	      struct dirent* file;
+	      
+	      /* Remove NUL-termination from last directory */
+	      if (p != unicorn_path)
+		*(p - 1) = ':';
+	      
+	      /* Find the character delimiting the current
+		 directory and the next directory */
+	      p_end = strchrnul(p, ':');
+	      /* NUL-terminate the current directory */
+	      *p_end = '\0';
+	      
+	      /* Opem the directory, skip to next on error */
+	      if ((dir = opendir(p)) == NULL)
+		continue;
+	      
+	      /* Compare all files in the directory agains the partial command name  */
+	      while ((file = readdir(dir)) != NULL)
+		/* Check that the file name starts with the partial name of the command */
+		if (!strcmp(file->d_name, command))
+		  /* Check that the found command does not match those so
+		     ridiculously included . and .. */
+		  if (strcmp(file->d_name, ".") && strcmp(file->d_name, ".."))
+		    {
+		      struct stat attr;
+		      mode_t mode;
+		      
+		      /* Concatenate the directory and the file */
+		      sprintf(pathname, "%s/%s", p, file->d_name);
+		      
+		      /* Get file stat, ignore on failure (removed or broken link)  */
+		      if (stat(pathname, &attr))
+			continue;
+		      
+		      /* Check that the file (symlinks followed) an
+			 executable (by anypony) regulare file (keep
+			 in mind, directories are normally executable) */
+		      mode = attr.st_mode;
+		      if (S_ISREG(mode) && ((S_IXUSR | S_IXGRP | S_IXOTH) & mode))
+			{
+			  /* We found it! */
+			  if ((command_file = strdup(pathname)) == NULL)
+			    {
+			      perror(*argv);
+			      abort();
+			    }
+			  printf("%s\n", file->d_name);
+			  break;
+			}
+		    }
+	      
+	      /* Close the directory */
+	      closedir(dir);
+	    }
+	  /* Remove NUL-termination from last directory */
+	  if (p != unicorn_path)
+	    *(p - 1) = ':';
+	}
+      if (command_file == NULL)
+	{
+	  fprintf(stderr, "%s: error: command not found: %s\n", *argv, command);
+	  return 1;
+	}
+      
+      /* Modify the environment if -p was not used */
+      if (preserve_env == 0)
+	if (putenv(unicorn_path_env))
+	  perror(*argv);
+      
+      /* Execute command */
+      {
+	/* NULL-terminate argument list */
+	char** command_argv = malloc((command_argc + 1) * sizeof(char*));
+	memcpy(command_argv, command_args, command_argc * sizeof(char*));
+	*(command_argv + command_argc) = NULL;
+	/* Exec */
+	execv(command_file, command_args);
+	perror(*argv);
+	abort();
+      }
+      return 2;
     }
   
   return 0;
